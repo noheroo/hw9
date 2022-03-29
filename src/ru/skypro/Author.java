859f525de1a083a36300bc6a7524f8d25507d0ca
nohero_ <serega1552@yandex.ru>
2022-03-29 23:06:12 +0400

    minimal level complete

---
+++ src/ru/skypro/Author.java
@@ -0,0 +1,27 @@
+package ru.skypro;
+
+public class Author {
+    private String lastName;
+    private String firstName;
+
+    public Author (String lastName, String firstName) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
---
+++ src/ru/skypro/Book.java
@@ -0,0 +1,40 @@
+package ru.skypro;
+
+public class Book {
+    private String name;
+    private Author author;
+    private int year;
+
+    public Book(String name, Author author, int year) {
+        this.name = name;
+        this.author = author;
+        this.year = year;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
+
+
+
