94b82b161284fc3b77c5ff64136408acf789086d
nohero_ <serega1552@yandex.ru>
2022-04-02 22:31:22 +0400

    medium level complete

--- src/ru/skypro/Book.java
+++ src/ru/skypro/Book.java
@@ -34,6 +34,9 @@ public class Book {
     public void setYear(int year) {
         this.year = year;
     }
+    public String toString() {
+        return this.author + ": " + this.name + ": " + this.year;
+    }
 }
 
 
--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,8 +3,12 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
+        //easy
         Author asprin = new Author("Asprin", "Robert");
         Author perumov = new Author("Perumov", "Nik");
+        Author king = new Author("King", "Stephen");
+        Author pushkin = new Author("Pushkin", "Alexander");
+        Author gogol = new Author("Gogol", "Nikolay");
 
         perumov.setFirstName("Nick");
 
@@ -16,5 +20,68 @@ public class Main {
         System.out.println("\nMinimal level");
         System.out.printf("Название: %s. Год публикации: %d. Автор: %s.%n", book1.getName(), book1.getYear(), book1.getAuthor());
         System.out.printf("Название: %s. Год публикации: %d. Автор: %s.%n", book2.getName(), book2.getYear(), book2.getAuthor());
+        System.out.println();
+
+        //medium
+        System.out.println("Medium level");
+
+        Book[] arrayBooks = new Book[7];
+
+        addBooks(arrayBooks,book1);
+        addBooks(arrayBooks,book2);
+
+        Book book3 = new Book("The green mile", king, 1996);
+        Book book4 = new Book("Ruslan and Ludmila", pushkin, 1820);
+        Book book5 = new Book("Dead souls", gogol, 1842);
+
+
+        addBooks(arrayBooks, book3);
+        addBooks(arrayBooks, book5);
+        addBooks(arrayBooks, book4);
+
+        printArray(arrayBooks);
+
+    }
+    public static void printArray(Book[] array) {
+        System.out.println("Вывод массива");
+        for (Book book : array) {
+            if (book != null) {
+                System.out.println(book);
+            }
+        }
+    }
+
+    public static void addBooks(Book[] array, Book plusBook) {
+        if (isFull(array)) {
+            System.out.println("Невозможно добавить книгу " + plusBook + ", в массиве нет места");
+            return;
+        }
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == null) {
+                array[i] = plusBook;
+                return;
+            }
+        }
+    }
+
+    public static boolean isFull(Book[] array) {
+        boolean full = false;
+        for (Book book : array) {
+            if (book == null) {
+                full = false;
+            } else {
+                full = true;
+            }
+        }
+        return full;
     }
 }
+
+
+
+
+
+
+
+
+
